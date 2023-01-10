c9beac37dfb8e8c05f24b9df0918da353b44a5a3
Mariam Imran <MARIIMRAN@my.lonestar.edu>
2023-01-10 01:00:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = "Hello World".ToCharArray();
+            ReverseString(s);
+            Console.WriteLine(new string(s));
+        }
+  
+        static void ReverseString(char[] s)
+        {
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int right = s.Length - i - 1; 
+                char temp;
+
+                if (i < right)
+                {
+                    temp = s[right];
+                    s[right] = s[i];
+                    s[i] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
